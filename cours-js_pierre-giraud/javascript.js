fb703efac1fe5635bffeb9efd600f174fe1c3cda
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-01-01 13:04:51 +0100

    Day 1 - Cours PJ,  bases de JS (variables, typologie,opération, concaténation

---
+++ cours-js_pierre-giraud/javascript.js
@@ -0,0 +1,10 @@
+
+    console.log("Hello World");
+    alert("Ceci est affiché en JavaScript");
+    alert("Une première instruction");
+    //alert(2023);
+
+    // Je suis un commentaire Monàligne
+
+    /* Je suis un commentaire
+                            mulitiligne  */
