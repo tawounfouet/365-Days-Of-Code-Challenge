c8edf78444ef40b71e87d923f2de6f316ef95502
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-01-12 01:59:18 +0100

    cours JS FromScratch : le DOM suite et fin

--- fromscratch_frontend-mastery/cours-javascript/1_cours_dom/script.js
+++ fromscratch_frontend-mastery/cours-javascript/1_cours_dom/script.js
@@ -146,7 +146,7 @@ const nav = document.querySelector("nav");
 
 window.addEventListener('scroll', () => {
     //console.log("Test !")
-    console.log(window.scrollY)
+    //console.log(window.scrollY)
 
     if (window.scrollY > 120) {
         nav.style.top = 0
@@ -162,9 +162,11 @@ window.addEventListener('scroll', () => {
 
 // ------------------------------------------------------------------
 
- const inputName = document.querySelector('input[type="text"]')
+const inputName = document.querySelector('input[type="text"]')
  //console.log(inputName);
- const select = document.querySelector("select")
+const select = document.querySelector("select")
+//console.log(inputName);
+const form = document.querySelector("form")
 
 let  pseudo = "";
 let language = "";
@@ -173,12 +175,226 @@ let language = "";
     //console.log("ohhh yes");
     //console.log(e.target.value);
     pseudo = e.target.value;
-    console.log(pseudo);
+    //console.log(pseudo);
     
  })
 
  select.addEventListener("input", (e) => {
     //console.log(e)
     language = e.target.value
-    console.log(language)
+    //console.log(language)
  })
+
+ form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //console.log("Le formulaire a été soumis")
+    //console.log(cgv.checked);
+
+    if (cgv.checked) {
+        // Afficher le contenu des variables
+        //document.querySelector('form > div').textContent
+        document.querySelector('form > div').innerHTML = `
+            <h3>Pseudo : ${pseudo}</h3>
+            <h4>Langage préféré : ${language}</h4>
+        `;
+    } else {
+        alert("Veuillez accepter les CGV")
+    }
+ });
+
+
+
+
+
+ // ----------------------------------------------------------------------
+ // Load Event 
+ window.addEventListener("load", () => {
+    //console.log("Document Chargé !")
+ })
+
+
+
+
+ // ----------------------------------------------------------------------
+ // ForEach 
+ const boxes = document.getElementsByClassName("box")
+ //console.log(boxes)
+
+ /*
+ boxes.addEventListener('click', () => console.log('test'))
+
+-->  Uncaught TypeError: boxes.addEventListener is not a function
+On ne peut pas ajouter un event listiner sur trois éléments à la fois 
+ */
+
+/*
+boxes.forEach((box) => {
+    box.addEventListener('click', (e) => {
+        console.log(e.target)
+    });
+});
+*/
+
+boxesQuery = document.querySelectorAll(".box");
+//console.log(boxesQuery);
+
+boxesQuery.forEach((box) => {
+    box.addEventListener('click', (e) => {
+        //console.log(e.target)
+        e.target.style.transform = "scale(0.7)";
+    });
+});
+
+
+//---------------------------------------------------------------------
+// addEventListener vs onClick
+// document.body.onclick = function () {
+//     console.log("Click !");
+// };
+
+// document.body.onscroll = () => {
+//     console.log("Scroll !");
+// };
+
+// Bubbling => fin (de base l'eventlistener est paramétré en mode Bubbling)
+document.addEventListener("click", 
+    () => {
+        console.log("click 1")
+    },
+    false
+);
+
+// Usecapture -- Pour passer devant tous les évènements
+document.addEventListener("click", 
+    () => {
+        console.log("click 2")
+    },
+    true
+);
+
+// Pour en savoir plus sur le 3e argument true/false
+// https://gomakethings.com/what-is-that-third-argument-on-the-vanilla-js-addeventlistener-method-and-when-do-you-need-it/#:~:text=That%20third%20argument%2C%20true%20%3F,That's%20called%20useCapture%20.
+
+
+
+// ----------------------------------------------------------------------
+// Stop propagation
+questionContainer.addEventListener('click', (e) => {
+    //alert("Test !");
+    e.stopPropagation();
+
+})
+
+// removeEventListiner
+
+
+
+
+
+// --------------------------------------------------------------------
+// BOM
+
+// console.log(window.innerHeight);
+// console.log(window.scrollY)
+// window.open("http://google.com", "cours js", height=600, width=800)
+// window.close()
+
+// confirm 
+btn2.addEventListener("click", () => {
+    confirm("Voulez vous vraiment vous tromper ?")
+})
+
+
+// prompt  
+btn1.addEventListener("click", () => {
+    //prompt("Entrez votre nom !");
+    let answer = prompt("Entrez votre nom !");
+
+    //questionContainer.innerHTML = "<h3>Bravo " + answer + "</h3>"
+    questionContainer.innerHTML += "<h3>Bravo " + answer + "</h3>"
+});
+
+// setTimeout
+setTimeout(() => {
+    // logique à exécuter
+    questionContainer.style.borderRadius = "300px"
+}, 2000); 
+
+
+// setIntervalle
+let interval = setInterval(() => {
+    document.body.innerHTML += `
+        <div class='box> 
+            <h2>Nouvelle Boite</h2>
+        </div>
+    `;
+}, 1000);
+
+document.body.addEventListener("click", () => {
+    //e.target.remove(); // retirer un élément du DOM
+    clearInterval(interval);
+});
+
+
+// Location
+// console.log(location.href)
+// console.log(location.host)
+// console.log(location.pathname)
+// console.log(location.search)
+// location.replace("https://awf-fitness.com")
+
+// window.onload = () {
+//     location.href = "http://awf-fitness.com"
+// }
+
+
+// Navigator 
+//console.log(navigator.userAgent);
+
+
+// Geolocatisation 
+
+//https://developer.mozilla.org/fr/docs/Web/API/Geolocation/getCurrentPosition
+
+/*
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Votre position actuelle est :');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude : ${crd.longitude}`);
+    console.log(`La précision est de ${crd.accuracy} mètres.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERREUR (${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+*/
+
+console.log(window.history)
+//window.history.back()
+// history.go(-2)
+
+
+// ---------------------------------------------------------------------
+const after = document.querySelector('.nav::after');
+
+console.log(after);
+
+
+// ----------------------------------------------------------------------
+// Set property
+window.addEventListener("mousemove", (e) => {
+    console.log(e)
+    nav.style.setProperty("--x", e.layerX, + "px")
+    nav.style.setProperty("--y", e.layerY, + "px")
+
+})
