7b97788c7be6e32850e81f7014988012fa427c72
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-01-15 20:51:25 +0100

    Day 15 - Crispy Forms tutorial form Django Mastery Youtube : Part #02 & 03

--- crispy-form_django-mastery/Template_Forms_Crispy/App/forms.py
+++ crispy-form_django-mastery/Template_Forms_Crispy/App/forms.py
@@ -1,13 +1,62 @@
 from django import forms
 from .models import Candidate
+from django.core.validators import RegexValidator
 
 
 class CandidateForm(forms.ModelForm):
 
+    # VALIDATIONS
+    firstname = forms.CharField(label='First name',
+                                min_length=3,
+                                max_length=50,
+                                validators=[RegexValidator(
+                                    '^[a-zA-ZÀ-ÿ\s]*$', message="Only letters is allowed !")],
+                                widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+
+    lastname = forms.CharField(label='Last name',
+                               min_length=3,
+                               max_length=50,
+                               validators=[RegexValidator(
+                                   '^[a-zA-ZÀ-ÿ\s]*$', message="Only letters is allowed !")],
+                               widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+
+    email = forms.CharField(label='Email address',
+                            min_length=3,
+                            max_length=50,
+                            validators=[RegexValidator(
+                                '^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', message="Put a valid email !")],
+                            widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+
+    # Method 1
+   # age = forms.CharField(widget=forms.TextInput(attrs={'type': 'number'}))
+
+    # Method 2
+    age = forms.CharField(label='Your age',
+                          validators=[RegexValidator(
+                                ' ^[0-9]*$', message="Only number is allowed !")],
+                          widget=forms.TextInput(attrs={'type': 'number'}))
+
+    message = forms.CharField(label='About you',
+                              min_length=10,
+                              max_length=500,
+                              required=False,
+                              validators=[RegexValidator(
+                                  '^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', message="Put a valid email !")],
+                              widget=forms.Textarea(attrs={'placeholder': 'Talk a little about you', 'rows': 10}))
+
     class Meta:
         model = Candidate
-        #fields = ["firstname", "lastname"]
+        #fields  = ["firstname", "lastname", "email", "age", "message"]
         #exclude = ["email", "message"]
         fields = "__all__"
 
+        # Outside Widget
+        widgets = {
+            'phone': forms.TextInput(
+                attrs={'style': 'font-size: 13px',
+                       'placeholder': 'Phone',
+                       'data-mask': '(+00) 00 00 00 00 00'
+                       }
+            )
 
+        }
--- crispy-form_django-mastery/Template_Forms_Crispy/App/models.py
+++ crispy-form_django-mastery/Template_Forms_Crispy/App/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Candidate(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
+    age = models.CharField(max_length=3)
+    phone = models.CharField(max_length=25) #(33) 668957467
     email = models.EmailField(max_length=50)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
